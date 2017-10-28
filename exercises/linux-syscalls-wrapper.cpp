29f7d85f2ceb92e355af6e2595d76ad41a40cbc1
Antonina Vertsinskaya <tyndria23@gmail.com>
2017-10-28 09:32:20 +0300

    Update linux-syscalls-wrapper.cpp

--- exercises/linux-syscalls-wrapper.cpp
+++ exercises/linux-syscalls-wrapper.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdio.h>
@@ -8,31 +9,14 @@
 
 using namespace std;
 
- char* getTokens(char *c) {
-    char *argv[64] = {};
-    int i = 0;
-    
-    char* token = strtok(c, " ");
-  
-    while( token != NULL ) {
-        argv[i] = token;
-        i++;
-        token = strtok(NULL, " ");
-    }
-    argv[i] = NULL;
-    
-    return *argv;
-}
-
-int main(int argc, char* arg_v[], char* env_p[])
-{
+int main(int argc, char* arg_v[], char* env_p[]) {
     int* status;
     pid_t my_pid;
     while (true) {
         string command;
         char* command_char;
         cout << "Please enter a syscall \n";
-        getline (cin, command);
+        getline(cin, command);
         command_char = strcpy((char*)malloc(command.length()+1), command.c_str());
         char *argv[64] = {};
         int i = 0;
@@ -54,8 +38,8 @@ int main(int argc, char* arg_v[], char* env_p[])
             cout << "exec: "<< argv[0] << endl;
             if (-1 == execve(argv[0], &argv[0], NULL)) {
                 perror("child process execve failed [%m]");
+                exit(0);
             } 
-            exit(0);
         } else {
             cout <<"wait" << endl;
             while (wait(status) == 0) {
